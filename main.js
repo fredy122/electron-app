0097d6f20e3a22d45aa8a7f8d209d0159761984b
fredy122 <i1413470@continental.edu.pe>
2022-07-27 12:43:21 -0500

    version 1.0.7

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 const { app, BrowserWindow, autoUpdater } = require('electron')
 
+const { version } = require('./package')
+
 const server = 'https://update.electronjs.org'
-const feed = `${server}/fredy122/electron-app/${process.platform}-${process.arch}/${app.getVersion()}`
+const feed = `${server}/fredy122/electron-app/${process.platform}/${version}`
+
+console.log(`Current version: ${version}`)
 
 autoUpdater.setFeedURL(feed)
+autoUpdater.checkForUpdates()
 
-setInterval(() => {
-    autoUpdater.checkForUpdates()
-  }, 1 * 60 * 1000)
+autoUpdater.on('checking-for-update', () => {
+  console.log('checking-for-update')
+})
+
+autoUpdater.on('update-available', () => {
+  console.log('update-available')
+})
+
+autoUpdater.on('update-not-available', () => {
+  console.log('update-not-available')
+})
+
+autoUpdater.on(
+  'update-downloaded',
+  (event, releaseNotes, releaseName, updateURL) => {
+    console.log('update-downloaded', {
+      event,
+      releaseNotes,
+      releaseName,
+      updateURL
+    })
+  }
+)
+
+autoUpdater.on('error', error => {
+  console.log('error', { error })
+})
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    show: false,
+    autoHideMenuBar: true,
+    movable: false,
+    resizable: false,
   })
 
   win.loadFile('index.html')
