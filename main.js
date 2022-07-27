3be6457fc8d259ed0137110325a85a2ea48797c5
fredy122 <i1413470@continental.edu.pe>
2022-07-27 13:00:40 -0500

    version 1.0.8

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
 const { app, BrowserWindow, autoUpdater } = require('electron')
 
-const { version } = require('./package')
+const server = 'https://electron-fqhm6tua8-fredy122.vercel.app'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
-const server = 'https://update.electronjs.org'
-const feed = `${server}/fredy122/electron-app/${process.platform}/${version}`
+autoUpdater.setFeedURL({ url })
+console.log(url);
 
-console.log(`Current version: ${version}`)
-
-autoUpdater.setFeedURL(feed)
-autoUpdater.checkForUpdates()
-
-autoUpdater.on('checking-for-update', () => {
-  console.log('checking-for-update')
-})
-
-autoUpdater.on('update-available', () => {
-  console.log('update-available')
-})
-
-autoUpdater.on('update-not-available', () => {
-  console.log('update-not-available')
-})
-
-autoUpdater.on(
-  'update-downloaded',
-  (event, releaseNotes, releaseName, updateURL) => {
-    console.log('update-downloaded', {
-      event,
-      releaseNotes,
-      releaseName,
-      updateURL
-    })
-  }
-)
-
-autoUpdater.on('error', error => {
-  console.log('error', { error })
-})
 
 const createWindow = () => {
   const win = new BrowserWindow({
