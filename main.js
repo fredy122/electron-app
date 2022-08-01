4339ab374424ae209c1ca85a1f7a2a9d250b2e4a
fredy122 <i1413470@continental.edu.pe>
2022-08-01 17:00:45 -0500

    prueba

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const { app, BrowserWindow, autoUpdater } = require('electron')
-
+/*
 const server = 'https://electron-app-eta.vercel.app'
 const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
 autoUpdater.setFeedURL({ url })
 console.log(url);
-
+*/
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    show: false,
+    show: true,
     autoHideMenuBar: true,
     movable: false,
     resizable: false,
